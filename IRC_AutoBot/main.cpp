13d422eb5b484d427fa8b4fbc80a02c33b49af67
Chris Amanse <chrisamanse@Chriss-MacBook-Pro-2.local>
2014-07-30 16:59:33 +0800

    Initial Commit

---
+++ IRC_AutoBot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  IRC_AutoBot
+//
+//  Created by Chris Amanse on 7/30/14.
+//  Copyright (c) 2014 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
